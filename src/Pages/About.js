f4b48532e6172e8db842403ee71963325a07bf65
SeckMohameth <mohamethseck34@gmail.com>
2022-05-02 01:20:35 -0400

    again

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -2,7 +2,7 @@ import {useState, useEffect} from 'react'
 
 
 function About() {
-  // state hello again
+  // state hello again and again
   const [count, setCount] = useState(0);
   const [up, setUp] = useState(0);
 
